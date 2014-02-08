18427594dd7da309c1d86cea8314adfb0b0e718a
igalmizrahi <igalmizrahi92@gmail.com>
2014-02-08 02:27:03 -0500

    server add method

--- server.js
+++ server.js
@@ -11,8 +11,25 @@ app.listen(8000);
 
 app.use('/static', express.static(__dirname + '/'));
 
+var people = []
+people.push( [ [10,50] , [20,70] , [30,60] , [50,60] ])
+people.push( [ [10,30] , [20,40] , [30,70] , [50,60] ])
+people.push( [ [10,20] , [20,0]  , [30,60] , [50,60] ])
+people.push( [ [10,70] , [20,0]  , [30,80] , [50,60] ])
 
+app.get('/coordinates', function(req, res) {
+	var base = parseInt(req.query.base) || 0
+	var basepoints = people[base]
+	var newarray = []
+	people.forEach( function(user, i) {
+		newarray.push( [] )
+		user.forEach( function(point, j) {
+			newarray[i].push( [ point[0] , Math.abs(point[1] - basepoints[j][1]) ] )
+		})
+	})
+	res.json(newarray)
+})
 
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
   	res.send(404,'nothing here');
 });
